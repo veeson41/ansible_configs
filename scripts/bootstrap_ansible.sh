f85cf5c7eebfddc8bc040cc8d3cb5b9ce750fa60
veeson <veeson41@gmail.com>
2023-12-21 17:34:34 +0000

    adding ansible installation, auto script

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+
+os_specific_package(){
+os=$(cat /etc/os-release | grep -w NAME | sed 's/NAME="//g' | sed 's/"//g' | cut -d " " -f1)
+[ "$os" = "Ubuntu" ]  && sudo apt update && sudo apt upgrade -y && sudo apt install ansible -y
+[ "$os" = "Arch" ]  && sudo pacman -Syu ansible --no-confirm
+[ "$os" = "Debian" ]  && sudo apt update && sudo apt upgrade -y && sudo apt install ansible -y
+}
+
+os_specific_package
 wget https://raw.githubusercontent.com/veeson41/ansible_configs/master/vars/raptor_secret && 
 ansible-vault decrypt raptor_secret && 
 sudo ansible-pull --vault-pass-file raptor_secret -U https://github.com/veeson41/ansible_configs && 
