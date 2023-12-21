837dcfc68a740b23bace064c547b6f098b4fa841
veeson <veeson41@gmail.com>
2023-12-21 16:30:44 +0000

    cleaning auto script for decryption

--- scripts/bootstrip_ansible.sh
+++ scripts/bootstrip_ansible.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-git clone https://github.com/veeson41/ansible_configs /
-&& ansible-vault decrypt ansible_configs/vars/raptor_secret /
-&& sudo ansible-pull --vault-pass-file ~/ansible_configs/vars/raptor_secret -U https://github.com/veeson41/ansible_configs /
-&& sudo rm -rf ansible_configs
+wget https://raw.githubusercontent.com/veeson41/ansible_configs/master/vars/raptor_secret && /
+ansible-vault decrypt raptor_secret && /
+sudo ansible-pull --vault-pass-file raptor_secret -U https://github.com/veeson41/ansible_configs && /
+sudo rm -rf raptor_secret
