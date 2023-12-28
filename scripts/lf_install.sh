ab88a103ce662836eba7f984497f34ae5d49c958
veeson <veeson41@gmail.com>
2023-12-28 15:28:29 +0000

    shell in automation, test

--- scripts/lf_install.sh
+++ scripts/lf_install.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
 
 env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
-sudo cp /home/veeson/go/bin/lf /usr/bin 
+#sudo cp /home/veeson/go/bin/lf /usr/bin 
 #sudo rm -rf /home/veeson/go
