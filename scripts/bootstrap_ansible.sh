db27f08d9e74cd3dddbaa51d19c2f0f6760cd308
veeson <veeson41@gmail.com>
2024-01-02 11:44:55 +0000

    specific local-pulls, test

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -11,26 +11,32 @@ debian(){
 sudo apt update && sudo apt upgrade -y && sudo apt install ansible -y
 }
 
-os_specific_package(){
+os_package_update(){
 	os=$(cat /etc/os-release | grep -w NAME | sed 's/NAME="//g' | sed 's/"//g' | cut -d " " -f1)
 	[ "$os" = "Ubuntu" ]  && debian
 	[ "$os" = "Arch" ]  && arch
 	[ "$os" = "Debian" ]  && debian
 }
 
+server_config(){
+wget https://raw.githubusercontent.com/veeson41/ansible_configs/master/vars/raptor_secret && 
+ansible-vault decrypt raptor_secret && 
+ansible-pull local-pulls/local-server.yml --check --vault-pass-file raptor_secret -U  https://github.com/veeson41/ansible_configs 
+}
+
 machine_type(){
   case $1 in
     server)
-      echo "serving";;
+      echo "Bootstrapping Server machine";;
     workstation)
-      echo "workstation";;
+      echo "Bootstrapping workstation";;
     *) 
-      echo "Not a configuration type..";;
+      echo "Not a configuration type";;
    esac
 }
 
+os_package_update
 machine_type $1
-#os_specific_package
 #wget https://raw.githubusercontent.com/veeson41/ansible_configs/master/vars/raptor_secret && 
 #ansible-vault decrypt raptor_secret && 
 #ansible-pull  --vault-pass-file raptor_secret -U https://github.com/veeson41/ansible_configs 
